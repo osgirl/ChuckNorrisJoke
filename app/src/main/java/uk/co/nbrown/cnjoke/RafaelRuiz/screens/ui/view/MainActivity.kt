ae11d168ecefd2c354a3e02d6a65cfbff21fa775
Rafael Ruiz Muñoz <raruizmu@gmail.com>
2017-11-08 16:08:26 +0000

    Initial project with libraries on

---
+++ app/src/main/java/uk/co/nbrown/cnjoke/RafaelRuiz/screens/ui/view/MainActivity.kt
@@ -0,0 +1,14 @@
+package uk.co.nbrown.cnjoke.RafaelRuiz.screens.ui.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import uk.co.nbrown.cnjoke.RafaelRuiz.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
